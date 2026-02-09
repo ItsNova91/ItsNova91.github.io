5bb4b6b87fcbf75fa5db513e8322ac6716015aa2
ItsNova91 <soynovayt25@gmail.com>
2026-02-08 19:41:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrar(id) {
+  document.querySelectorAll('.seccion').forEach(sec => {
+    sec.classList.remove('activa');
+  });
+  document.getElementById(id).classList.add('activa');
+}
